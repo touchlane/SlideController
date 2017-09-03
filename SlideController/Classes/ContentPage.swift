7bd33793dc90c39c65819da852dcc8a6530595b2
Dzmitry Navak <navakdzmitry@gmail.com>
2017-09-04 02:39:58 +0300

    ContentPage class description

--- SlideController/Classes/ContentPage.swift
+++ SlideController/Classes/ContentPage.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class ContentPage {
+///Represents container for one page content
+final internal class ContentPage {
+    
+    ///Used internally for pages ordering
     var constraints = [NSLayoutConstraint]()
     fileprivate var intenalView : UIView
     
+    /// - Parameter view: The view to show as content.
     init(view : UIView) {
         intenalView = view
     }
