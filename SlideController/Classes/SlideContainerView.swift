8a9248a188b68e65145195d4d4ccc27fb1213fe8
Evgeny Dedovets <ededovets@panda-systems.com>
2017-09-30 16:44:08 +0300

    no message

---
+++ SlideController/Classes/SlideContainerView.swift
@@ -0,0 +1,30 @@
+//
+//  SlideContainerView.swift
+//  SlideController
+//
+//  Created by Evgeny Dedovets on 4/25/17.
+//  Copyright © 2017 Panda Systems. All rights reserved.
+//
+
+import UIKit
+
+///Represents container view for one page content
+final class SlideContainerView {
+    private var internalView: UIView
+    
+    ///Constraints for internal view, allow to change UI positioning for container view
+    var constraints = [NSLayoutConstraint]()
+    
+    /// - Parameter view: The view to show as content.
+    init(view: UIView) {
+        internalView = view
+    }
+}
+
+///Viewable protocol implementation
+private typealias ViewableImplementation = SlideContainerView
+extension ViewableImplementation: Viewable {
+    var view: UIView {
+        return internalView
+    }
+}
