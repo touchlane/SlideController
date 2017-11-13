161149ab20dbda4e27258f981ca5362336c5669a
Pavel Kondrashkov <pkondrashkov@gmail.com>
2017-11-13 18:13:00 +0300

    test appending lifecycle

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -12,6 +12,7 @@ class Tests: XCTestCase {
             pagesContent: [],
             startPageIndex: 0,
             slideDirection: SlideDirection.horizontal)
+        
     }
     
     override func tearDown() {
@@ -19,12 +20,14 @@ class Tests: XCTestCase {
     }
     
     func testAppended() {
-        let page1 = SlidePageModel<PageLifeCycleObject>(object: PageLifeCycleObject())
-        let page2 = SlidePageModel<PageLifeCycleObject>(object: PageLifeCycleObject())
-        let page3 = SlidePageModel<PageLifeCycleObject>(object: PageLifeCycleObject())
+        let page1 = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
+        let page2 = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
+        let page3 = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
         let givenContent = [page1, page2, page3]
         slideController.append(object: givenContent)
-
+        slideController.viewDidAppear()
+        
+        
         let contentCount = slideController.content.count
         let currentIndex = slideController.content.index(where: {
             $0 === slideController.currentModel
@@ -32,5 +35,28 @@ class Tests: XCTestCase {
         
         XCTAssertEqual(contentCount, givenContent.count)
         XCTAssertEqual(currentIndex, 0)
+    }
+    
+    func testAppendedLifeCycle() {
+        let utils = TestLifeCycleObjectUtils()
+        let page1 = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
+        let page2 = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
+        let page3 = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
+        let page4 = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
+        let givenContent = [page1, page2, page3, page4]
+        slideController.append(object: givenContent)
+        slideController.viewDidAppear()
+        
+        guard let currentPage = slideController.currentModel?.lifeCycleObject as? TestableLifeCycleObject,
+            let secondPage = page2.lifeCycleObject as? TestableLifeCycleObject,
+            let thirdPage = page3.lifeCycleObject as? TestableLifeCycleObject,
+            let fourthPage = page4.lifeCycleObject as? TestableLifeCycleObject else {
+            XCTFail("Current page is not TestableLifeCycleObject")
+            return
+        }
+        utils.assertPresentedLifeCycleObject(object: currentPage, wasSlided: false)
+        utils.assertOutsideScreenLifeCycleObject(object: secondPage, shouldBeLoad: true)
+        utils.assertOutsideScreenLifeCycleObject(object: thirdPage, shouldBeLoad: false)
+        utils.assertOutsideScreenLifeCycleObject(object: fourthPage, shouldBeLoad: false)
     }
 }
