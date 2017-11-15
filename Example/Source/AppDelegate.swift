dc7ef0d301e59475748f02c05b7708696ae04d4c
Pavel Kondrashkov <pkondrashkov@gmail.com>
2017-11-15 10:45:39 +0300

    fixed status bar color for white background main view

--- Example/Source/AppDelegate.swift
+++ Example/Source/AppDelegate.swift
@@ -18,7 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = rootVC
-        UIApplication.shared.statusBarStyle = .lightContent
         router.presenter = rootVC
         router.openMainScreen(animated: true)
         window?.makeKeyAndVisible()
