c3f9ad895446342fb38066838148923945b8d5cb
Vadim Morozov <vmorozov@touchlane.com>
2017-11-21 11:24:04 +0300

    Add more load tests

--- Tests/LoadTests.swift
+++ Tests/LoadTests.swift
@@ -86,5 +86,55 @@ class LoadTests: BaseTestCase {
         XCTAssert(object3.viewDidUnloadTriggered)
     }
     
+    func testInsertOnContentLoadingModeDisabled() {
+        slideController.isContentUnloadingEnabled = false
+        let page1 = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
+        let page2 = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
+        let page3 = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
+        let givenContent = [page1, page2, page3]
+        slideController.append(object: givenContent)
+        
+        let insertingPage = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
+        slideController.insert(object: insertingPage, index: 0)
+        
+        guard let insertingObject = insertingPage.lifeCycleObject as? TestableLifeCycleObject,
+            let object1 = page1.lifeCycleObject as? TestableLifeCycleObject,
+            let object2 = page2.lifeCycleObject as? TestableLifeCycleObject,
+            let object3 = page3.lifeCycleObject as? TestableLifeCycleObject else {
+                XCTFail("page is not TestableLifeCycleObject")
+                return
+        }
+        
+        XCTAssert(insertingObject.viewDidLoadTriggered)
+        XCTAssert(!insertingObject.viewDidUnloadTriggered)
+        XCTAssert(!object1.viewDidUnloadTriggered)
+        XCTAssert(!object2.viewDidUnloadTriggered)
+        XCTAssert(!object3.viewDidUnloadTriggered)
+    }
     
+    func testAppendOnContentLoadingModeDisabled() {
+        slideController.isContentUnloadingEnabled = false
+        let page1 = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
+        let page2 = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
+        let page3 = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
+        let givenContent = [page1, page2, page3]
+        slideController.append(object: givenContent)
+        
+        let appendingPage = SlidePageModel<TestableLifeCycleObject>(object: TestableLifeCycleObject())
+        slideController.insert(object: appendingPage, index: 2)
+        
+        guard let appendingObject = appendingPage.lifeCycleObject as? TestableLifeCycleObject,
+            let object1 = page1.lifeCycleObject as? TestableLifeCycleObject,
+            let object2 = page2.lifeCycleObject as? TestableLifeCycleObject,
+            let object3 = page3.lifeCycleObject as? TestableLifeCycleObject else {
+                XCTFail("page is not TestableLifeCycleObject")
+                return
+        }
+        
+        XCTAssert(appendingObject.viewDidLoadTriggered)
+        XCTAssert(!appendingObject.viewDidUnloadTriggered)
+        XCTAssert(!object1.viewDidUnloadTriggered)
+        XCTAssert(!object2.viewDidUnloadTriggered)
+        XCTAssert(!object3.viewDidUnloadTriggered)
+    }
 }
